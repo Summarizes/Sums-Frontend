1d45d4ab83a82e834db280ca1377c4f45b01d2bf
Aduiduidui <thanakrit.earth@gmail.com>
2023-02-11 17:03:56 +0700

    down nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,15 +6,15 @@ import { Button, Nav, NavItem, NavLink } from "reactstrap";
 
 const Navbar = () => {
   return (
-    <Nav className="nav py-3 justify-between d-flex items-center">
+    <Nav className="nav py-1 justify-between d-flex items-center">
       <div className="d-flex items-center gap-x-5">
       <Link to={"/"}>
       <img
-        className="h-10 ml-4"
+        className="h-8 ml-4"
         src="https://cdn.discordapp.com/attachments/1073664493210579044/1073906306114662421/Sums_logo_v2.png"
       />
       </Link>
-      <NavLink href="/" className="text-white mt-2" ><h4>Home</h4></NavLink>
+      <NavLink href="/" className="text-white mt-2" ><h5>Home</h5></NavLink>
       </div>
       <NavItem className="d-flex items-center">
         <Link to={"/cart"} className="w-14 cart-but my-auto mr-4">
