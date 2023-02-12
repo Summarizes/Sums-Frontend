c78d72921701091a7713678a3334819f766b8c2c
PlaPud <patcub252545@gmail.com>
2023-02-12 11:51:07 +0700

    Homepage First Commit

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Container, Input } from 'reactstrap'
 
 const Homepage = () => {
   return (
-    <div>
-        {/* <h1>Home</h1> */}
+    <div className='mx-16 mt-16'>
+      <div className='text-left'>
+        <h3 className='ml-12 search-bundle-title p-3 inline'>Search Bundle</h3>
+      </div>
+      <div className='overflow-hidden search-body'>
+        <div className='w-1/3 mt-6 mx-auto'>
+          <Input className='search-bundle-bar'/>
+        </div>
+      </div>
     </div>
   )
 }
